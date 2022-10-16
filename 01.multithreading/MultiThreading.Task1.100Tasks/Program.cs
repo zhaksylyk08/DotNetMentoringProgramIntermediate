cbfb68d360560cc38f2f6e7ada531e1933e1d721
Zhadyra Zhaksylyk <zhadyra.zhaksylyk@wickes.co.uk>
2022-10-16 20:29:14 +0600

    Implemented the first task

---
+++ 01.multithreading/MultiThreading.Task1.100Tasks/Program.cs
@@ -0,0 +1,49 @@
+﻿/*
+ * 1.	Write a program, which creates an array of 100 Tasks, runs them and waits all of them are not finished.
+ * Each Task should iterate from 1 to 1000 and print into the console the following string:
+ * “Task #0 – {iteration number}”.
+ */
+using System;
+using System.Threading.Tasks;
+
+namespace MultiThreading.Task1._100Tasks
+{
+    class Program
+    {
+        const int TaskAmount = 100;
+        const int MaxIterationsCount = 1000;
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(".Net Mentoring Program. Multi threading V1.");
+            Console.WriteLine("1.	Write a program, which creates an array of 100 Tasks, runs them and waits all of them are not finished.");
+            Console.WriteLine("Each Task should iterate from 1 to 1000 and print into the console the following string:");
+            Console.WriteLine("“Task #0 – {iteration number}”.");
+            Console.WriteLine();
+            
+            HundredTasks();
+
+            Console.ReadLine();
+        }
+
+        static void HundredTasks()
+        {
+            for (int i = 0; i < TaskAmount; i++) {
+                var taskNumber = i + 1;
+
+                Task.Run(() =>
+                {
+                    for (int j = 0; j < MaxIterationsCount; j++)
+                    {
+                        Output(taskNumber, j + 1);
+                    }
+                });
+            }
+        }
+
+        static void Output(int taskNumber, int iterationNumber)
+        {
+            Console.WriteLine($"Task #{taskNumber} – {iterationNumber}");
+        }
+    }
+}
