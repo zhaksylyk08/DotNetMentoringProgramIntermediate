73e8e669ea6eb09f00946fe290ea41aa3abee93e
Zhadyra Zhaksylyk <zhadyra.zhaksylyk@wickes.co.uk>
2022-10-30 20:51:39 +0600

    Added task 5

--- 01.multithreading/MultiThreading.Task5.Threads.SharedCollection/Program.cs
+++ 01.multithreading/MultiThreading.Task5.Threads.SharedCollection/Program.cs
@@ -5,21 +5,57 @@
  * Use Thread, ThreadPool or Task classes for thread creation and any kind of synchronization constructions.
  */
 using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Threading;
 
 namespace MultiThreading.Task5.Threads.SharedCollection
 {
     class Program
     {
-        static void Main(string[] args)
+        private static List<int> numbers; 
+        private static readonly object numbersLock = new object();
+
+        static void Main()
         {
             Console.WriteLine("5. Write a program which creates two threads and a shared collection:");
             Console.WriteLine("the first one should add 10 elements into the collection and the second should print all elements in the collection after each adding.");
             Console.WriteLine("Use Thread, ThreadPool or Task classes for thread creation and any kind of synchronization constructions.");
             Console.WriteLine();
 
-            // feel free to add your code
+            numbers = new List<int>();
+
+            //ThreadPool.QueueUserWorkItem(AddNumbers);
+            //ThreadPool.QueueUserWorkItem(Print);
+
+            new Thread(AddNumbers).Start();
 
             Console.ReadLine();
+        }
+
+        private static void AddNumbers(object state) {
+            for (int i = 0; i < 10; i++) {
+                lock (numbersLock)
+                {
+                    numbers.Add(i + 1);
+                    Console.WriteLine($"{i + 1} added");
+                }
+
+                new Thread(Print).Start();
+
+            }
+        }
+
+        private static void Print(object state)
+        {
+            lock (numbersLock) {
+                foreach (var item in numbers)
+                {
+                    Console.Write($"{item} ");
+                }
+
+                Console.WriteLine();
+            }
         }
     }
 }
