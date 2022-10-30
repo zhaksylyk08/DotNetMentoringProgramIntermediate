dc2d5dcbccb27928714a688377ed8ce3bcc76334
Zhadyra Zhaksylyk <zhadyra.zhaksylyk@wickes.co.uk>
2022-10-30 23:17:31 +0600

    Added task 6

--- 01.multithreading/MultiThreading.Task6.Continuation/Program.cs
+++ 01.multithreading/MultiThreading.Task6.Continuation/Program.cs
@@ -7,6 +7,8 @@
    Demonstrate the work of the each case with console utility.
 */
 using System;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace MultiThreading.Task6.Continuation
 {
@@ -22,7 +24,56 @@ namespace MultiThreading.Task6.Continuation
             Console.WriteLine("Demonstrate the work of the each case with console utility.");
             Console.WriteLine();
 
-            // feel free to add your code
+            #region a. Continuation task should be executed regardless of the result of the parent task.
+            Task.Run(() => {
+                Console.WriteLine("Hello from the parent task!");
+            })
+            .ContinueWith(parent => {
+                Console.WriteLine($"Continuation task run when the parent task's status is {parent.Status}");
+             })
+            .Wait();
+            #endregion
+
+            #region b. Continuation task should be executed when the parent task finished without success.
+            Task.Run(() => {
+                Console.WriteLine("Throwing exception from parent task.");
+                throw new Exception("Throwing exception from parent task.");
+            })
+            .ContinueWith(parent => {
+                Console.WriteLine($"Continuation task run when the parent task's status is {parent.Status}");
+            }, TaskContinuationOptions.NotOnRanToCompletion)
+            .Wait();
+            #endregion
+
+            #region c. Continuation task should be executed when the parent task would be finished with fail and parent task thread should be reused for continuation.
+            Task.Run(() => {
+                Console.WriteLine("Throwing exception from parent task.");
+                throw new Exception("Throwing exception from parent task.");
+            })
+            .ContinueWith(parent => {
+                Console.WriteLine($"Continuation task run when the parent task's status is {parent.Status}");
+            }, TaskContinuationOptions.OnlyOnFaulted)
+            .Wait();
+            #endregion
+
+            #region d. Continuation task should be executed outside of the thread pool when the parent task would be cancelled. 
+            var tokenSource = new CancellationTokenSource();
+            var token = tokenSource.Token;
+
+            var task = Task.Run(() => {
+                if (token.IsCancellationRequested) {
+                    token.ThrowIfCancellationRequested();
+                }
+            }, token);
+
+            task.ContinueWith(parent =>
+            {
+                Console.WriteLine($"Continuation task run when the parent task's status is {parent.Status}");
+            }, TaskContinuationOptions.OnlyOnCanceled);
+
+            tokenSource.Cancel();
+
+            #endregion
 
             Console.ReadLine();
         }
