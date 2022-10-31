6edcdb0073f28189e46824e2ef9b35968acce5a1
Zhadyra Zhaksylyk <zhadyra.zhaksylyk@wickes.co.uk>
2022-10-31 13:53:09 +0600

    Added task 2

---
+++ 02.AsyncAwait.Tasks/AsyncAwait.Tasks/AsyncAwait.Task1.CancellationTokens/Calculator.cs
@@ -0,0 +1,21 @@
+﻿using System.Threading;
+
+namespace AsyncAwait.Task1.CancellationTokens;
+
+internal static class Calculator
+{
+    // todo: change this method to support cancellation token
+    public static long Calculate(int n /*, CancellationToken token*/)
+    {
+        long sum = 0;
+
+        for (var i = 0; i < n; i++)
+        {
+            // i + 1 is to allow 2147483647 (Max(Int32)) 
+            sum = sum + (i + 1);
+            Thread.Sleep(10);
+        }
+
+        return sum;
+    }
+}
