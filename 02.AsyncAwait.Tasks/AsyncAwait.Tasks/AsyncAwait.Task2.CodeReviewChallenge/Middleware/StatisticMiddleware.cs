f6b3bf563b059d9f663f665286fcd33bd769a41d
Zhadyra Zhaksylyk <zhadyra.zhaksylyk@wickes.co.uk>
2022-11-14 13:12:17 +0600

    Refactored StatisticMiddleware

--- 02.AsyncAwait.Tasks/AsyncAwait.Tasks/AsyncAwait.Task2.CodeReviewChallenge/Middleware/StatisticMiddleware.cs
+++ 02.AsyncAwait.Tasks/AsyncAwait.Tasks/AsyncAwait.Task2.CodeReviewChallenge/Middleware/StatisticMiddleware.cs
@@ -22,21 +22,10 @@ public class StatisticMiddleware
     public async Task InvokeAsync(HttpContext context)
     {
         string path = context.Request.Path;
-
-        var staticRegTask = Task.Run(
-            () => _statisticService.RegisterVisitAsync(path)
-                .ConfigureAwait(false)
-                .GetAwaiter().OnCompleted(UpdateHeaders));
-        Console.WriteLine(staticRegTask.Status); // just for debugging purposes
-
-        void UpdateHeaders()
-        {
-            context.Response.Headers.Add(
-                CustomHttpHeaders.TotalPageVisits,
-                _statisticService.GetVisitsCountAsync(path).GetAwaiter().GetResult().ToString());
-        }
-
-        Thread.Sleep(3000); // without this the statistic counter does not work
+        await _statisticService.RegisterVisitAsync(path);
+        var totalPageVisits = await _statisticService.GetVisitsCountAsync(path);
+        context.Response.Headers.Add(
+                CustomHttpHeaders.TotalPageVisits, totalPageVisits.ToString());
         await _next(context);
     }
 }
