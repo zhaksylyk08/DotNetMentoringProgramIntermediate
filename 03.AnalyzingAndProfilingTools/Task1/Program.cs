a239df35d3e18a5dc867144ecb4d7c5ad822c970
Zhadyra Zhaksylyk <zhadyrazhaksylyk08@gmail.com>
2022-11-27 16:59:05 +0600

    Added HW3 Task1

---
+++ 03.AnalyzingAndProfilingTools/Task1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System.Security.Cryptography;
+
+namespace MyProject;
+class Program
+{
+    static void Main(string[] args)
+    {
+        var password = "test12345";
+        var passwordHash = GeneratePasswordHashUsingSalt(password);
+
+        Console.WriteLine(passwordHash);
+    }
+    public static string GeneratePasswordHashUsingSalt(string passwordText)
+    {
+        var iterate = 10000;
+        var salt = new byte[16];
+        
+        using (var rng = RandomNumberGenerator.Create()) {
+            rng.GetBytes(salt);
+        }
+
+        var pbkdf2 = new Rfc2898DeriveBytes(passwordText, salt, iterate);
+        byte[] hash = pbkdf2.GetBytes(20);
+
+        var hashBytes = new List<byte>(salt);
+        hashBytes.AddRange(hash);
+
+        var passwordHash = Convert.ToBase64String(hashBytes.ToArray());
+
+        return passwordHash;
+
+    }
+}
+
+
